7f716a902fc13b03c6dbf473fc7d057e05f21124
Josué Vicioso <tdelfino2509@gmail.com>
2018-05-15 23:48:36 -0400

    Initial Commit

---
+++ src/main/java/com/example/fts/FtsApplication.java
@@ -0,0 +1,12 @@
+package com.example.fts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtsApplication.class, args);
+	}
+}
